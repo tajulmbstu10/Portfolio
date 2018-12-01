430fb893ef75778653758ee3c47db9c81d1a85f1
tajulmbstu10 <tajulmbstu@gmail.com>
2018-12-01 20:07:05 +0600

    Updates

---
+++ precache-manifest.59e9dbffc3ad344b194e77fd0a4d4ab3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eca2248e8e975369e1a",
+    "url": "/Portfolio/static/css/main.99132bc8.chunk.css"
+  },
+  {
+    "revision": "2eca2248e8e975369e1a",
+    "url": "/Portfolio/static/js/main.2eca2248.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/Portfolio/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "c971321c88df96b3e01bb0d03630b0b8",
+    "url": "/Portfolio/index.html"
+  }
+];
